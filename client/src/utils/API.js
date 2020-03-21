3d90ecea09367066a8ec729b51d7779fe1fdfd90
MDBA <artes.mike@gmail.com>
2020-03-20 22:02:52 -0400

    adding updated api.js

---
+++ client/src/utils/API.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const query ="https://www.googleapis.com/books/v1/volumes?q="
+const apiKey = process.env.API_KEY
+
+export default {
+    searchBooks: function (input) {
+        return axios.get(query + input.split(" ").join("+") + apiKey)
+    },
+    getBooks: function () {
+        return axios.get("/api/books");
+
+    },
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
+
+    },
+
+    saveBook: function (bookData) {
+        return axios.post("/api/books", bookData)
+    }
+}
