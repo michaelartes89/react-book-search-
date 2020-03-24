01cdec58e2030ff8ebe4e034011ed6d17b498801
MDBA <artes.mike@gmail.com>
2020-03-24 18:59:29 -0400

    adding production build

---
+++ client/build/precache-manifest.9b8511959d5561cac46f501cdad9fb29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38849bc4fd63b40e421b2d7793bce22d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11cb756de01a529f8ed9",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7f99cadbd21291665f62",
+    "url": "/static/css/main.e4ed0e63.chunk.css"
+  },
+  {
+    "revision": "11cb756de01a529f8ed9",
+    "url": "/static/js/2.4290725f.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.4290725f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f99cadbd21291665f62",
+    "url": "/static/js/main.61557fef.chunk.js"
+  },
+  {
+    "revision": "19d437767c354b85b265",
+    "url": "/static/js/runtime-main.c059e870.js"
+  }
+]);
