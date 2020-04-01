db7cec49468d9f141d205992de97a24e39a00aa3
MDBA <artes.mike@gmail.com>
2020-03-31 20:27:26 -0400

    switched mongoDB connection from localhost to mlab for heroku

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
+  // process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
+  process.env.MONGODB_URI || "mongodb://michael:password1@ds031895.mlab.com:31895/heroku_v70lthb7"
+
+
 );
 
 // Start the API server
