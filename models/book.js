edf1572d1735c89add8113cf5fc04aa401deea24
MDBA <artes.mike@gmail.com>
2020-03-31 20:21:52 -0400

    fixed error in bookScema

--- models/book.js
+++ models/book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
     title:{ type: String, required: true},
     authors: {type: Array, required: true},
-    descriptions: {type: String, required: true },
+    description: {type: String, required: true },
     image: { type: String, required: true},
     link: { type: String, required: true}
 
